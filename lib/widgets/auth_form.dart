0f2d995790aedb9b0746956fe02fa33401888bac
pmakaruk <makar1998@yahoo.pl>
2021-04-05 23:42:58 +0200

    Add validation for auth screen and signup mode

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -7,6 +7,25 @@ class AuthenticationForm extends StatefulWidget {
 }
 
 class _AuthenticationFormState extends State<AuthenticationForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userPassword = '';
+  var _userName = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState.save();
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,34 +34,80 @@ class _AuthenticationFormState extends State<AuthenticationForm> {
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.all(16),
-            child: Form(child: Column(mainAxisSize: MainAxisSize.min,
-             children: <Widget>[
-               TextFormField(
-                 keyboardType: TextInputType.emailAddress,
-                 decoration: InputDecoration(
-                   labelText: 'Email address',
-                 ),
-               ),
-               TextFormField(
-                 decoration: InputDecoration(
-                   labelText: 'Username',
-                 )
-               ),
-               TextFormField(
-                 decoration: InputDecoration(
-                   labelText: 'Password'),
-                 obscureText: true,
-               ),
-               SizedBox(height: 12,),
-               ElevatedButton(child: Text('Login'), onPressed: () {},),
-               TextButton(child: Text('Create new account'), onPressed: () {},)
-               ],
-            ))
+            child: Form(
+              key: _formKey, 
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    key: ValueKey('userEmail'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Email address can not be empty.';
+                      }
+                      else if(!value.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email address',
+                    ),
+                    onSaved: (value) {
+                      _userEmail = value;
+                    },
+                  ),
+                  TextFormField(
+                    key: ValueKey('userName'),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Username can not be empty.';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                    ),
+                    onSaved: (value) {
+                      _userName = value;
+                    },
+                  ),
+                  TextFormField(
+                    key: ValueKey('userPassword'),
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 8) {
+                        return 'Password must be at least 8 characters long.';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Password'),
+                    obscureText: true,
+                    onSaved: (value) {
+                      _userPassword = value;
+                    },
+                  ),
+                  SizedBox(height: 12,),
+                  ElevatedButton(
+                    child: Text(_isLogin ? 'Log in' : 'Sign up'), 
+                    onPressed: _trySubmit
+                  ),
+                  TextButton(
+                    child: Text(_isLogin ? 'Create new account' : 'I already have an account'), 
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                  )
+                ],
+              )
+            )
           ),
         ),
       ),
     );
-
-      
   }
 }
